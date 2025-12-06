27a00d06aca9f5fc58e540790357cc12f8d46b5f
natnael <natnaelaster42@gmail.com>
2025-12-06 01:51:51 -0800

    answering quastions
    answering analytical quastions with mysql

---
+++ day_4_HR ANALYTICS & EMPLOYEE ATTRITION/employe_hr.sql
@@ -0,0 +1,7 @@
+-- Create database called (supply_&_chain_dashboard)
+create database `hr_analytics`;
+use `hr_analytics`;
+-- ------------------------------------------------------------------------------------------------------
+-- Import dataset from local file
+select * from `hr-employee-attrition`;
+
