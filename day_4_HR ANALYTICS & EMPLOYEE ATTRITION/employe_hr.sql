148747d6530c7bd40d5b7963dfb693e4ed045219
natnael <natnaelaster42@gmail.com>
2025-12-13 01:11:01 -0800

    answering
    answering analytical quastions

--- day_4_HR ANALYTICS & EMPLOYEE ATTRITION/employe_hr.sql
+++ day_4_HR ANALYTICS & EMPLOYEE ATTRITION/employe_hr.sql
@@ -5,3 +5,20 @@ use `hr_analytics`;
 -- Import dataset from local file
 select * from `hr-employee-attrition`;
 
+-- 1. Overall attrition rate and by department
+
+select 
+	'Department',
+    count(*),
+    sum(case when 'Attrition' = 'Yes' then 1 else 0 end) as total_attrition,
+	round(sum(case when 'Attrition' = 'Yes' then 1 else 0 end) / count(*) * 100, 2) as attrition_rate
+    from `hr-employee-attrition`
+    group by 'Department'
+    order by attrition_rate;
+    
+alter table `hr-employee-attrition`
+add column attrition_num int
+	 when Attrition = 'Yes' then 1,
+			 when Attrition = 'Yes' then 0
+after Attrition
+		
