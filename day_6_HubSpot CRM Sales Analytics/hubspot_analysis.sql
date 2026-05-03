6546acf81f662d54ea71f8327181a8dc70e737ce
Natnael Birhanu <natnaelaster42@gmail.com>
2026-05-03 02:13:44 -0700

    sql

--- day_6_HubSpot CRM Sales Analytics/hubspot_analysis.sql
+++ day_6_HubSpot CRM Sales Analytics/hubspot_analysis.sql
@@ -1,26 +1,6 @@
 use sql_projects;
 
-select * from hubspot_sales_data;
+select * from hubspot_crm_data;
 
-CREATE TABLE hubspot_crm_data (
-    ID INT PRIMARY KEY,
-    Sales_agent VARCHAR(100),
-    Product_name VARCHAR(100),
-    Company_name VARCHAR(150),
-    Deal_stage VARCHAR(50),
-    Engage_date DATE,
-    Close_date DATE,
-    Close_value DECIMAL(12,2),
-    Sector VARCHAR(100),
-    Year_established INT,
-    Revenue DECIMAL(15,2),
-    Employees INT,
-    Office VARCHAR(100),
-    Product_series VARCHAR(100),
-    Sales_price DECIMAL(12,2),
-    Manager VARCHAR(100),
-    Regional_office VARCHAR(100)
-);
-ALTER TABLE hubspot_crm_data
-MODIFY COLUMN Year_established datetime;
-     
+select count(*) from hubspot_crm_data;
+
