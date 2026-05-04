7a1c750496accf3f4dd6dd696994f4dd4979cab1
Natnael Birhanu <natnaelaster42@gmail.com>
2026-05-03 23:38:49 -0700

    next step

--- day_6_HubSpot CRM Sales Analytics/hubspot_analysis.sql
+++ day_6_HubSpot CRM Sales Analytics/hubspot_analysis.sql
@@ -1,6 +1,34 @@
+-- <<<<<<<<<<<<<<<<< HubSpot CRM Sales Analytics >>>>>>>>>>>>>>>>>
+
+-- This SQL script is designed to analyze the HubSpot CRM sales data. 
+-- It retrieves all records from the `hubspot_crm_data` table and counts the total number of records present in that table.
+
+-- Database: sql_projects
+-- Table: hubspot_crm_data
+
+-- Data Exploration
+
 use sql_projects;
+
+-- Step 1: 
+--Select all records from the `hubspot_crm_data` table
 
 select * from hubspot_crm_data;
 
+-- Step 2: 
+--Count the total number of records in the `hubspot_crm_data` table
+
 select count(*) from hubspot_crm_data;
 
+-- Data Analysis
+
+
+
+
+
+
+
+
+
+
+-- End of SQL script for HubSpot CRM Sales Analytics
